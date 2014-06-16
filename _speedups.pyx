eb627e3e884934d5dd74bed6e2460a9cc5815de4
Sergei Lebedev <superbobry@gmail.com>
2014-06-16 17:48:27 +0400

    Added 'header' argument to 'write_fasta'

--- _speedups.pyx
+++ _speedups.pyx
@@ -7,12 +7,16 @@ from itertools import groupby
 from toolshed import nopen, is_newer_b
 
 
-def fasta_write(fh, bytes text, int width=100):
-    cdef int i
-    cdef int offset
-    for offset in range(0, len(text), width):
+def fasta_write(fh, bytes header, bytes seq, int width=100):
+    cdef:
+        int i
+        int offset
+    fh.write(">")
+    fh.write(header)
+    fh.write(os.linesep)
+    for offset in range(0, len(seq), width):
         for i in range(width):
-            fh.write(text[i + offset])
+            fh.write(seq[i + offset])
         fh.write(os.linesep)
 
 
@@ -30,7 +34,7 @@ def fasta_iterread(fh):
             chunks.add(line)
 
 
-def convert_fasta(ref_fasta, just_name=False):
+def convert_fasta(bytes ref_fasta, just_name=False):
     out_fasta = ref_fasta + ".bwameth.c2t"
     if just_name:
         return out_fasta
@@ -45,10 +49,8 @@ def convert_fasta(ref_fasta, just_name=False):
     oh = open(out_fasta, "wb")
     try:
         for header, seq in fasta_iterread(ih):
-            oh.write(">r%s\n" % header)  # Reverse
-            fasta_write(oh, seq.replace("G", "A"))
-            oh.write(">f%s\n" % header)  # Forward
-            fasta_write(oh, seq.replace("C", "T"))
+            fasta_write(oh, "r" + header, seq.replace("G", "A"))  # Reverse.
+            fasta_write(oh, "f" + header, seq.replace("C", "T"))  # Forward.
     finally:
         ih.close()
         oh.close()
